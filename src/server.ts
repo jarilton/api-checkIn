326d70f9ba60b3f5729a125e445eacd428d9e863
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2023-12-09 16:05:01 -0300

    chore:structure initial project

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { app }  from "./app";
+
+app.listen({
+    host: '0.0.0.0',
+    port: 3333
+}).then((address) => {
+    console.log(`Server listening at ${address}`);
+}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+})
