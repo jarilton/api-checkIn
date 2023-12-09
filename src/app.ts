c46343358fcead55c4825ed3436b5f6b1664d2f6
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2023-12-09 17:33:23 -0300

    chore: instalando e configurando o prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    email: '    ',
+    name: '    ',
+    password: '    ',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+})
