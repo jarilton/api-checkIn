ff8d13ae1e2dd001a1c58bd6c496972daa930672
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2023-12-20 23:19:14 -0300

    chore: inversão de dependencia

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,27 +1,31 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
-import { z } from 'zod'
-import { registerUseCase } from '@/services/register'
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+import { RegisterUseCase } from "@/services/register";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
-  })
+  });
 
-  const { name, email, password } = registerBodySchema.parse(request.body)
+  const { name, email, password } = registerBodySchema.parse(request.body);
 
   try {
-    await registerUseCase({
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
+
+    await registerUseCase.execute({
       name,
       email,
       password,
-    })
+    });
   } catch (error) {
-    return reply.status(400).send()
+    return reply.status(400).send();
   }
 
-  return reply.status(201).send()
+  return reply.status(201).send();
 }
--- src/services/register.ts
+++ src/services/register.ts
@@ -1,35 +1,33 @@
-import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
-import { hash } from 'bcryptjs'
+import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
+import { hash } from "bcryptjs";
 
 interface RegisterUseCaseProps {
-  name: string
-  email: string
-  password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseProps) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+  async execute({ name, email, password }: RegisterUseCaseProps) {
+    const password_hash = await hash(password, 6);
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new Error("User with same email already exists");
+    }
+
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('User with same email already exists')
+      password: password_hash,
+    });
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password: password_hash,
-  })
 }
