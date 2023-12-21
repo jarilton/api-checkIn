79b7ba93eb6fb2081e7630c26729379814f93188
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2023-12-20 22:37:06 -0300

    chore: caso de uso de registro

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { registerUseCase } from '@/services/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,27 +13,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send({
-      error: 'User with this email already exists',
-    })
-  }
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password: password_hash,
-    },
-  })
+      password,
+    })
+  } catch (error) {
+    return reply.status(400).send()
+  }
 
   return reply.status(201).send()
 }
