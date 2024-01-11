5907a701af15d950badd2901b6ea627c3232d840
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-10 23:18:13 -0300

    chore: TDD & mocking

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,6 +1,4 @@
-import { hash } from "bcryptjs";
-
-import { expect, describe, it, beforeEach } from "vitest";
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
 import { CheckInUseCase } from "./check-in";
 
@@ -11,6 +9,12 @@ describe("Check in use case", () => {
   beforeEach(() => {
     checkInRepository = new InMemoryCheckInsRepository();
     sut = new CheckInUseCase(checkInRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to check in", async () => {
@@ -19,7 +23,40 @@ describe("Check in use case", () => {
       gymId: "gym-id",
     });
 
+    console.log(checkIn.created_at);
+
     expect(checkIn.id).toEqual(expect.any(String));
   });
-  
+
+  it("should not be able to check in twice in the same day", async () => {
+    vi.setSystemTime(new Date("2024-01-10 10:00:00"));
+
+    await sut.execute({
+      userId: "user-id",
+      gymId: "gym-id",
+    });
+
+    await expect(
+      sut.execute({
+        userId: "user-id",
+        gymId: "gym-id",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+
+  it("should be able to check in twice but in different day", async () => {
+    vi.setSystemTime(new Date("2024-01-11 10:00:00"));
+
+    await sut.execute({
+      userId: "user-id",
+      gymId: "gym-id",
+    });
+
+    const { checkIn } = await sut.execute({
+      userId: "user-id",
+      gymId: "gym-id",
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
 });
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,31 +2,37 @@ import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 
 interface CheckInUseCaseRequest {
-  userId: string
-  gymId: string
+  userId: string;
+  gymId: string;
 }
 
 interface CheckInUseCaseResponse {
-  checkIn: CheckIn
+  checkIn: CheckIn;
 }
 
 export class CheckInUseCase {
-  constructor(
-    private checkInsRepository: CheckInsRepository,
-  ) { }
-  
+  constructor(private checkInsRepository: CheckInsRepository) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
+      userId,
+      new Date()
+    );
+
+    if (checkInOnSameDate) {
+      throw new Error();
+    }
+
     const checkIn = await this.checkInsRepository.create({
       user_id: userId,
       gym_id: gymId,
-    })
+    });
 
     return {
       checkIn,
-    }
+    };
   }
 }
