934669880e941cfe24748cb994cb95c0b87857b4
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-18 00:11:52 -0300

    chore: validando horário do check-in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,9 +1,8 @@
 import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
-import { InMemoryGymsRepository } from "@/repositories/in-memory/in-memory-gyms-repository";
-import { CheckInUseCase } from "./check-in";
 import { ValidateCheckInUseCase } from "./validate-check-in";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationsError } from "./errors/late-check-in-validation-error";
 
 let checkInRepository: InMemoryCheckInsRepository;
 let sut: ValidateCheckInUseCase;
@@ -13,18 +12,17 @@ describe("Validate Check in use case", () => {
     checkInRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckInUseCase(checkInRepository);
 
-    // vi.useFakeTimers();
+    vi.useFakeTimers();
   });
 
-  //   afterEach(() => {
-  //     vi.useRealTimers();
-  //   });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
   it("should be able to validate the check-in", async () => {
     const createdCheckIn = await checkInRepository.create({
       gym_id: "gym-01",
       user_id: "user-01",
-      validade_at: null,
     });
 
     const { checkIn } = await sut.execute({
@@ -41,5 +39,24 @@ describe("Validate Check in use case", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+    await expect(
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(LateCheckInValidationsError);
   });
 });
