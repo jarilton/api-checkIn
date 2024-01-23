b0ff4529fa576bda9d9220afadf4a642c6260cc9
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-22 23:28:55 -0300

    chore: implementando JWT no fastify

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,17 +1,18 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.string().default('development'),
+  NODE_ENV: z.string().default("development"),
+  JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
-})
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-  console.error('Invalid environment variables', _env.error.format())
+  console.error("Invalid environment variables", _env.error.format());
 
-  throw new Error(_env.error.message)
+  throw new Error(_env.error.message);
 }
 
-export const env = _env.data
+export const env = _env.data;
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -1,4 +1,4 @@
-import { InvalidCredentialsError } from './../../use-cases/errors/invalid-credentials-error';
+import { InvalidCredentialsError } from "./../../use-cases/errors/invalid-credentials-error";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import { z } from "zod";
@@ -17,9 +17,22 @@ export async function authenticate(
   try {
     const authenticaUseCase = makeAuthenticateUseCase();
 
-    await authenticaUseCase.execute({
+    const { user } = await authenticaUseCase.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
     });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
@@ -30,6 +43,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
