954f733769dc3ffc84ddc41c80e9844626eafec8
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-26 00:14:48 -0300

    chore: teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,24 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("Ok", () => {});
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const response = await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      password: "123456",
+    });
+
+    expect(response.status).toBe(201);
+    // expect(response.body).toHaveProperty("id");
+  });
+});
