8ce9808863306029d706228087aeddb545326ef2
jariltonpereira <jarilton.pereira@grupofocus.com.br>
2024-01-28 17:15:25 -0300

    chore: controller de criação de academia

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,4 +1,4 @@
-import { InvalidCredentialsError } from "./../../use-cases/errors/invalid-credentials-error";
+import { InvalidCredentialsError } from "../../../use-cases/errors/invalid-credentials-error";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
 import { z } from "zod";
